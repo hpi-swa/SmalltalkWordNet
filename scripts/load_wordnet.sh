3cd32e6c5ed52c09a7e2496cccfe3be2bcd32787
Patrick R <patrick.rein@gmail.com>
2017-05-21 22:07:46 +0200

    Fixes newlines in script

--- scripts/load_wordnet.sh
+++ scripts/load_wordnet.sh
@@ -1,1 +1,9 @@
-#!/bin/bashWORDNET_URL="http://wordnetcode.princeton.edu/3.0/WNprolog-3.0.tar.gz"wget $WORDNET_URLtar -xf WNprolog-3.0.tar.gzmv prolog ${SMALLTALK_CI_BUILD}/wordnet -Rexit 0
+#!/bin/bash
+
+WORDNET_URL="http://wordnetcode.princeton.edu/3.0/WNprolog-3.0.tar.gz"
+
+wget $WORDNET_URL
+tar -xf WNprolog-3.0.tar.gz
+mv prolog ${SMALLTALK_CI_BUILD}/wordnet -R
+
+exit 0
