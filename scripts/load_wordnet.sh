7b60496921eb4381be4268ebc4a9270664d354d7
Patrick R <patrick.rein@gmail.com>
2017-05-21 22:40:59 +0200

    Removes unused and wrong -r flag from load script

--- scripts/load_wordnet.sh
+++ scripts/load_wordnet.sh
@@ -4,6 +4,6 @@ WORDNET_URL="http://wordnetcode.princeton.edu/3.0/WNprolog-3.0.tar.gz"
 
 wget $WORDNET_URL
 tar -xf WNprolog-3.0.tar.gz
-mv prolog ${SMALLTALK_CI_BUILD}/wordnet -R
+mv prolog ${SMALLTALK_CI_BUILD}/wordnet 
 
 exit 0
