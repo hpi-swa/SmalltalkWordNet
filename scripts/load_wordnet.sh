22d2be25734f5da8828415494baeb4daf1e0238f
Patrick R <patrick.rein@gmail.com>
2017-05-22 10:02:03 +0200

    Fixes the build script

--- scripts/load_wordnet.sh
+++ scripts/load_wordnet.sh
@@ -5,6 +5,4 @@ WORDNET_URL="http://wordnetcode.princeton.edu/3.0/WNprolog-3.0.tar.gz"
 wget $WORDNET_URL
 tar -xzf WNprolog-3.0.tar.gz
 mkdir -p ${SMALLTALK_CI_BUILD}/wordnet
-mv prolog/ ${SMALLTALK_CI_BUILD}/wordnet 
-
-exit 0
+mv prolog/* ${SMALLTALK_CI_BUILD}/wordnet 
