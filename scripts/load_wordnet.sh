4e2207344af3a850fe28e24406bc8b7ea343a053
Patrick R <patrick.rein@gmail.com>
2017-05-22 09:24:11 +0200

    Fixes path creation method

--- scripts/load_wordnet.sh
+++ scripts/load_wordnet.sh
@@ -4,7 +4,7 @@ WORDNET_URL="http://wordnetcode.princeton.edu/3.0/WNprolog-3.0.tar.gz"
 
 wget $WORDNET_URL
 tar -xzf WNprolog-3.0.tar.gz
-mkdir ${SMALLTALK_CI_BUILD}/wordnet
+mkdir -p ${SMALLTALK_CI_BUILD}/wordnet
 mv prolog/ ${SMALLTALK_CI_BUILD}/wordnet 
 
 exit 0
